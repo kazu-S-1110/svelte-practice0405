2f1ca3f242f412824b70eeab88ac4d5d5d5b9193
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-04-05 21:02:58 +0900

    tailwindをインストール

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,45 +1,50 @@
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+  import Counter from '$lib/Counter.svelte';
 </script>
 
-<main>
-	<h1>Hello world!</h1>
+<main class="bg-black">
+  <h1>Hello world!</h1>
+  <p class="text-xl text-white">Tailwind is enabled</p>
 
-	<Counter />
+  <Counter />
 
-	<p>Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
+  <p>
+    Visit <a class="text-blue-600 underline" href="https://svelte.dev"
+      >svelte.dev</a
+    > to learn how to build Svelte apps.
+  </p>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
+<style style lang="postcss">
+  main {
+    @apply text-center;
+    @apply p-4;
+    @apply mx-auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4rem;
-		font-weight: 100;
-		line-height: 1.1;
-		margin: 4rem auto;
-		max-width: 14rem;
-	}
+  h1 {
+    @apply text-red-600;
+    @apply uppercase;
+    @apply text-6xl;
+    @apply font-thin;
+    @apply leading-tight;
+    @apply my-16 mx-auto;
+    @apply max-w-xs;
+  }
 
-	p {
-		max-width: 14rem;
-		margin: 2rem auto;
-		line-height: 1.35;
-	}
+  p {
+    @apply max-w-xs;
+    @apply my-8 mx-auto;
+    @apply leading-snug;
+  }
 
-	@media (min-width: 480px) {
-		h1 {
-			max-width: none;
-		}
+  @screen sm {
+    h1 {
+      @apply max-w-none;
+    }
 
-		p {
-			max-width: none;
-		}
-	}
+    p {
+      @apply max-w-none;
+    }
+  }
 </style>
